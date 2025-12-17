8151b2958e4d20fbde43e9844c28b3d7a645126b
root <root@backend-kmp.N1>
2025-12-17 23:09:00 +0000

    from backend changes

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
--- CREATE DATABASE pgk_food;
--- INSERT INTO users (id, login, password_hash, role, name, surname)
--- VALUES ("uuid-1","oleg","$2a$12$SdxOWmwo3EC5ePbG6Bd4gexRltEyfwezigD5lP8tPVIr1IeiMTxd.","ADMIN", "Олег", "Владиславович")
+CREATE DATABASE pgk_food;
+INSERT INTO users (id, login, password_hash, role, name, surname)
+VALUES ("uuid-1","oleg","$2a$12$IWrMXckMwhBeZgfGg.ywMul3pz9eHKYzyEYjfO2qjrh6yrYll53Vy","ADMIN", "Олег", "Владиславович")
